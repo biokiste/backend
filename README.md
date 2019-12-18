fc9af0a8a3d6b024f2ce03efe4e08cdbd43dfd9d
Sebastian Koslitz <sebastian.koslitz@gmail.com>
2019-12-18 17:32:27 +0100

    add sql db connection with demo query

--- README.md
+++ README.md
@@ -3,5 +3,7 @@
 
 ## development
 
+expects running mysql instance on `localhost:8889` (root, root) with db foodkoop_biokiste
+
 - compile backend with `go build`
 - run backend with `./backend`
--- handlers.go
+++ handlers.go
@@ -1,16 +1,50 @@
 package main
 
 import (
+	"database/sql"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
+
+// UsersResponse JSON API Spec Wrapper
+type UsersResponse struct {
+	Users []User `json:"data"`
+}
+
+// User holds properties
+type User struct {
+	ID       int    `json:"id"`
+	Username string `json:"username"`
+}
 
 // ShowStatus delivers actual status
 func ShowStatus(w http.ResponseWriter, r *http.Request) {
-	printJSON(w, "biokiste api status: ok")
+	printJSON(w, "ok")
 }
 
 // ListUsers delivers user data
 func ListUsers(w http.ResponseWriter, r *http.Request) {
-	loc := "TODO: should list users"
-	printJSON(w, loc)
+	db, err := sql.Open("mysql", "root:root@tcp(127.0.0.1:8889)/foodkoop_biokiste")
+	if err != nil {
+		printError(w, err.Error())
+	}
+	defer db.Close()
+
+	results, err := db.Query("SELECT id, username FROM users")
+	if err != nil {
+		printError(w, err.Error())
+	}
+
+	var users []User
+	for results.Next() {
+		var user User
+		err = results.Scan(&user.ID, &user.Username)
+		if err != nil {
+			printError(w, err.Error())
+		}
+		users = append(users, user)
+	}
+
+	printJSON(w, &UsersResponse{Users: users})
 }
