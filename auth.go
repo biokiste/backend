7ac36d675c2faf99c19c48a125dc0ae1592f1899
Sebastian Koslitz <sebastian.koslitz@gmail.com>
2020-02-05 22:46:30 +0100

    Implement Authentication in backend #3

--- auth.go
+++ auth.go
@@ -1,63 +1,36 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
-	"strings"
 
-	"github.com/spf13/viper"
+	auth0 "github.com/auth0-community/go-auth0"
+	"gopkg.in/square/go-jose.v2"
 )
 
 // Auth define routes that need authentication
-func Auth(inner http.Handler) http.Handler {
+func Auth(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		token, _ := readTokenFromHeader(r)
+		auth0Domain := "https://biokiste.eu.auth0.com/"
+		jwkClient := auth0.NewJWKClient(auth0.JWKClientOptions{URI: fmt.Sprintf("%s.well-known/jwks.json", auth0Domain)}, nil)
+		validator := auth0.NewValidator(auth0.NewConfiguration(jwkClient, []string{}, auth0Domain, jose.RS256), nil)
+		token, err := validator.ValidateRequest(r)
 
 		switch {
 		case "/api/status" == r.RequestURI:
-			inner.ServeHTTP(w, r)
+			next.ServeHTTP(w, r)
 		case "/api/contents" == r.RequestURI:
-			inner.ServeHTTP(w, r)
+			next.ServeHTTP(w, r)
 		default:
-			_, err := ValidateToken(token)
 			if err != nil {
-				//inner.ServeHTTP(w, r)
-				notAuthorized(w, "not authorized")
+				fmt.Println(err)
+				fmt.Println("Token is not valid:", token)
+				w.WriteHeader(http.StatusUnauthorized)
+				w.Write([]byte("Unauthorized"))
 			} else {
-				inner.ServeHTTP(w, r)
+				next.ServeHTTP(w, r)
 			}
 		}
 
 	})
-}
-
-// ValidateToken validates request token
-func ValidateToken(tokenString string) (bool, error) {
-
-	APIKey := viper.GetString("apikey")
-
-	// check if token is not empty
-	if tokenString == "" {
-		fmt.Println("empty token")
-		return false, errors.New("empty token")
-	}
-	if tokenString != APIKey {
-		return false, errors.New("wrong credentials")
-	}
-	return true, nil
-}
-
-func readTokenFromHeader(r *http.Request) (string, error) {
-	authHeader := r.Header.Get("Authorization")
-	if authHeader == "" {
-		return "", nil // empty token
-	}
-
-	authHeaderParts := strings.Split(authHeader, " ")
-	if len(authHeaderParts) != 2 || strings.ToLower(authHeaderParts[0]) != "bearer" {
-		return "", errors.New("Authorization header format must be Bearer {token}")
-	}
-
-	return authHeaderParts[1], nil
 }
--- go.mod
+++ go.mod
@@ -3,8 +3,12 @@ module github.com/biokiste/backend
 go 1.13
 
 require (
+	github.com/auth0-community/go-auth0 v1.0.0
+	github.com/auth0/go-jwt-middleware v0.0.0-20190805220309-36081240882b
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/go-sql-driver/mysql v1.4.1
 	github.com/gorilla/mux v1.7.3
 	github.com/spf13/viper v1.6.1
 	google.golang.org/appengine v1.6.5 // indirect
+	gopkg.in/square/go-jose.v2 v2.4.1
 )
