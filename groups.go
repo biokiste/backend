7f9883c78a4032a06376a3970cc1dbdc3d08afc2
Peter Holló <lordpejdar@gmail.com>
2020-03-25 13:39:10 +0100

    Provide route to update group
    Closes #43

--- groups.go
+++ groups.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"strings"
+
+	"github.com/gorilla/mux"
 )
 
 // GetGroupsRoutes get all routes of path /groups
@@ -14,6 +17,12 @@ func GetGroupsRoutes(h *Handlers) []Route {
 			"POST",
 			"/groups",
 			h.addGroup,
+		},
+		{
+			"update group",
+			"PATCH",
+			"/groups/{id}",
+			h.updateGroupByID,
 		},
 	}
 
@@ -71,6 +80,65 @@ func (h *Handlers) addGroup(w http.ResponseWriter, r *http.Request) {
 	}
 
 	printJSON(w, &resBody{"ok", int(id)})
+}
+
+func (h *Handlers) updateGroupByID(w http.ResponseWriter, r *http.Request) {
+	type body struct {
+		Key           string `json:"key,omitempty"`
+		Email         string `json:"amount,omitempty"`
+		UpdatedBy     int    `json:"updatedBy"`
+		UpdateComment string `json:"updateComment"`
+	}
+
+	id, _ := mux.Vars(r)["id"]
+
+	var b body
+	err := json.NewDecoder(r.Body).Decode(&b)
+	if err != nil {
+		fmt.Println(err)
+		printInternalError(w)
+		return
+	}
+
+	if b.UpdatedBy == 0 || b.UpdateComment == "" {
+		code := 400
+		msg := "Some required fields are missing!"
+		fmt.Println(msg)
+		printCustomError(w, ErrorMessage{code, msg}, code)
+		return
+	}
+
+	var str strings.Builder
+
+	fmt.Fprint(&str, "UPDATE Groups SET ")
+	if b.Key != "" {
+		fmt.Fprintf(&str, `GroupKey = "%s", `, b.Key)
+	}
+	if b.Email != "" {
+		fmt.Fprintf(&str, "Email = %s, ", b.Email)
+	}
+
+	fmt.Fprintf(&str, `UpdatedAt = CURRENT_TIMESTAMP(), UpdatedBy = %d, UpdateComment = "%s" WHERE ID = %s`, b.UpdatedBy, b.UpdateComment, id)
+
+	query := str.String()
+
+	result, err := h.DB.Exec(query)
+
+	if err != nil {
+		fmt.Println(err)
+		printInternalError(w)
+		return
+	}
+
+	rowsAffected, _ := result.RowsAffected()
+
+	type resBody struct {
+		Status       string `json:"status"`
+		RowsAffected int    `json:"rowsAffected"`
+	}
+
+	printJSON(w, &resBody{"ok", int(rowsAffected)})
+
 }
 
 // // GetGroupsWithUsers return groups with user ids and ids of group leaders
