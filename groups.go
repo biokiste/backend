788f2dd664e279fd65f1456f66361fcc15ff8f62
Peter Holló <lordpejdar@gmail.com>
2020-03-26 16:52:33 +0100

    Provide route to get groups
    Closes #40

--- groups.go
+++ groups.go
@@ -24,9 +24,35 @@ func GetGroupsRoutes(h *Handlers) []Route {
 			"/groups/{id}",
 			h.updateGroupByID,
 		},
+		{
+			"get groups",
+			"GET",
+			"/groups",
+			h.getGroups,
+		},
 	}
 
 	return routes
+}
+
+type group struct {
+	ID            int    `json:"id"`
+	Key           string `json:"key"`
+	Email         string `json:"email"`
+	CreatedAt     string `json:"createdAt"`
+	CreatedBy     int    `json:"createdBy"`
+	UpdatedAt     string `json:"updatedAt,omitempty"`
+	UpdatedBy     int    `json:"updatedBy,omitempty"`
+	UpdateComment string `json:"updateComment,omitempty"`
+	Users         []int  `json:"users"`
+	Leaders       []int  `json:"leaders"`
+}
+
+type groupUser struct {
+	ID       int
+	GroupID  int
+	UserID   int
+	IsLeader int
 }
 
 func (h *Handlers) addGroup(w http.ResponseWriter, r *http.Request) {
@@ -138,7 +164,78 @@ func (h *Handlers) updateGroupByID(w http.ResponseWriter, r *http.Request) {
 	}
 
 	printJSON(w, &resBody{"ok", int(rowsAffected)})
+}
 
+func (h *Handlers) getGroups(w http.ResponseWriter, r *http.Request) {
+	query := "SELECT ID, GroupKey, Email, CreatedAt, CreatedBy, COALESCE(UpdatedAt, '') AS UpdatedAt, COALESCE(UpdatedBy, 0) AS UpdatedBy, COALESCE(UpdateComment, '') AS UpdateComment FROM Groups"
+	results, err := h.DB.Query(query)
+
+	if err != nil {
+		fmt.Println(err)
+		printInternalError(w)
+		return
+	}
+	defer results.Close()
+
+	var groups []group
+	for results.Next() {
+		var g group
+		err := results.Scan(
+			&g.ID,
+			&g.Key,
+			&g.Email,
+			&g.CreatedAt,
+			&g.CreatedBy,
+			&g.UpdatedAt,
+			&g.UpdatedBy,
+			&g.UpdateComment,
+		)
+		if err != nil {
+			fmt.Println(err)
+			printInternalError(w)
+			return
+		}
+
+		q := fmt.Sprintf("SELECT ID, GroupID, UserID, IsLeader FROM GroupUsers WHERE GroupID = %d", g.ID)
+		r, err := h.DB.Query(q)
+		if err != nil {
+			fmt.Println(err)
+			printInternalError(w)
+			return
+		}
+		defer r.Close()
+		for r.Next() {
+			var gu groupUser
+			err := r.Scan(
+				&gu.ID,
+				&gu.GroupID,
+				&gu.UserID,
+				&gu.IsLeader,
+			)
+			if err != nil {
+				fmt.Println(err)
+				printInternalError(w)
+				return
+			}
+			g.Users = append(g.Users, gu.UserID)
+			if gu.IsLeader == 1 {
+				g.Leaders = append(g.Leaders, gu.UserID)
+			}
+		}
+		if len(g.Users) == 0 {
+			g.Users = make([]int, 0)
+		}
+		if len(g.Leaders) == 0 {
+			g.Leaders = make([]int, 0)
+		}
+		groups = append(groups, g)
+	}
+
+	if len(groups) == 0 {
+		groups = make([]group, 0)
+	}
+
+	printJSON(w, &groups)
 }
 
 // // GetGroupsWithUsers return groups with user ids and ids of group leaders
